b4983b542a1f34df484df4ddfcc187db9af1d697
Ashish Prajapati <ashishanshprajapati@gmail.com>
2026-03-02 14:15:21 +0000

    refactor(queue): add manual insertion tracking to queue manager
    Introduce QueuedTrack struct to wrap TrackMetadata with manual_added flag,
    add last_manual_insert_index field to track manual insert positions,
    implement insert_after_current method for precise track insertion,
    and update queue operations to work with QueuedTrack structure

--- src/queue/manager.rs
+++ src/queue/manager.rs
@@ -2,9 +2,16 @@ use crate::audio::metadata::TrackMetadata;
 use log::info;
 use std::collections::VecDeque;
 
+#[derive(Clone)]
+pub struct QueuedTrack {
+    pub track: TrackMetadata,
+    pub manually_added: bool,
+}
+
 pub struct QueueManager {
-    queue: VecDeque<TrackMetadata>,
+    queue: VecDeque<QueuedTrack>,
     current_index: Option<usize>,
+    last_manual_insert_index: Option<usize>,
 }
 
 impl QueueManager {
@@ -12,26 +19,64 @@ impl QueueManager {
         Self {
             queue: VecDeque::new(),
             current_index: None,
+            last_manual_insert_index: None,
         }
     }
 
-    pub fn add(&mut self, track: TrackMetadata) {
-        info!("Adding to queue: {}", track.title);
-        self.queue.push_back(track);
+    pub fn add(&mut self, track: TrackMetadata, manually_added: bool) {
+        info!(
+            "Adding to queue (manual={}): {}",
+            manually_added, track.title
+        );
+        self.queue.push_back(QueuedTrack {
+            track,
+            manually_added,
+        });
 
         if self.current_index.is_none() {
             self.current_index = Some(0);
         }
     }
 
-    #[allow(dead_code)]
-    pub fn add_multiple(&mut self, tracks: Vec<TrackMetadata>) {
+    pub fn add_multiple(&mut self, tracks: Vec<TrackMetadata>, manually_added: bool) {
         for track in tracks {
-            self.add(track);
+            self.add(track, manually_added);
         }
     }
 
-    pub fn remove(&mut self, index: usize) -> Option<TrackMetadata> {
+    pub fn insert_after_current(&mut self, track: TrackMetadata) {
+        let insert_pos = if let Some(last_manual) = self.last_manual_insert_index {
+            if let Some(current) = self.current_index {
+                if last_manual >= current {
+                    last_manual + 1
+                } else {
+                    current + 1
+                }
+            } else {
+                last_manual + 1
+            }
+        } else {
+            self.current_index.map(|i| i + 1).unwrap_or(0)
+        };
+
+        self.queue.insert(
+            insert_pos,
+            QueuedTrack {
+                track,
+                manually_added: true,
+            },
+        );
+
+        self.last_manual_insert_index = Some(insert_pos);
+
+        if let Some(current) = self.current_index {
+            if insert_pos <= current {
+                self.current_index = Some(current + 1);
+            }
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) -> Option<QueuedTrack> {
         if index < self.queue.len() {
             let track = self.queue.remove(index)?;
 
@@ -49,24 +94,31 @@ impl QueueManager {
         }
     }
 
-    #[allow(dead_code)]
     pub fn clear(&mut self) {
         self.queue.clear();
         self.current_index = None;
+        self.last_manual_insert_index = None;
     }
 
-    pub fn next_track(&mut self) -> Option<&TrackMetadata> {
+    pub fn next_track(&mut self) -> Option<&QueuedTrack> {
         if let Some(current) = self.current_index {
             let next_index = current + 1;
             if next_index < self.queue.len() {
                 self.current_index = Some(next_index);
+
+                if let Some(last_manual) = self.last_manual_insert_index {
+                    if next_index > last_manual {
+                        self.last_manual_insert_index = Some(next_index);
+                    }
+                }
+
                 return self.queue.get(next_index);
             }
         }
         None
     }
 
-    pub fn previous(&mut self) -> Option<&TrackMetadata> {
+    pub fn previous(&mut self) -> Option<&QueuedTrack> {
         if let Some(current) = self.current_index {
             if current > 0 {
                 self.current_index = Some(current - 1);
@@ -76,7 +128,7 @@ impl QueueManager {
         None
     }
 
-    pub fn current(&self) -> Option<&TrackMetadata> {
+    pub fn current(&self) -> Option<&QueuedTrack> {
         if let Some(index) = self.current_index {
             self.queue.get(index)
         } else {
@@ -88,7 +140,7 @@ impl QueueManager {
         self.current_index
     }
 
-    pub fn get_track(&self, index: usize) -> Option<&TrackMetadata> {
+    pub fn get_track(&self, index: usize) -> Option<&QueuedTrack> {
         self.queue.get(index)
     }
 
@@ -100,7 +152,6 @@ impl QueueManager {
         self.queue.is_empty()
     }
 
-    #[allow(dead_code)]
     pub fn move_up(&mut self, index: usize) -> bool {
         if index > 0 && index < self.queue.len() {
             self.queue.swap(index, index - 1);
@@ -119,7 +170,6 @@ impl QueueManager {
         }
     }
 
-    #[allow(dead_code)]
     pub fn move_down(&mut self, index: usize) -> bool {
         if index + 1 < self.queue.len() {
             self.queue.swap(index, index + 1);
